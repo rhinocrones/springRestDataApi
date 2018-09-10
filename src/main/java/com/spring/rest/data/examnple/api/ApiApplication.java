119380e4df80f44727dffafcca74c9a7b54b0afa
Volodymyr Pedko <vpedk@softserveinc.com>
2018-09-10 09:59:33 +0300

    Frist commit

---
+++ src/main/java/com/spring/rest/data/examnple/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package com.spring.rest.data.examnple.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
